3bf7f72cda0f735b562c43ef579df0953fb38fa7
Ed Sanders <ejsanders@gmail.com>
2019-07-25 14:38:23 +0100

    Add placeholder

--- demo.js
+++ demo.js
@@ -2,7 +2,8 @@ $( function () {
 	var widget = new mw.widgets.TitleInputWidget( {
 		api: new mw.ForeignApi( mw.config.get( 'wgScriptPath' ) + '/api.php', { anonymous: true } ),
 		showImages: true,
-		showDescriptions: true
+		showDescriptions: true,
+		placeholder: 'Search for a Wikipedia page'
 	} );
 	$( document.body ).append( widget.$element );
 } );
